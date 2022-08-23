c81ab43f01a7a7e1b8fa5dbece05ecc24e9f2187
Nakhyeon <nakhe90@gmail.com>
2022-08-23 00:35:04 -0700

    bootstrap test

--- membertest.php
+++ membertest.php
@@ -5,51 +5,66 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <title>table</title>
 </head>
 <body>
-    <main>
-        <div class="User_Table">
-                <?php
-                    function tabledisplay($details){
-                        return "<tr><td>$details->id</td><td>$details->first_name</td><td>$details->last_name</td><td>$details->user_type</td><td>$details->username</td><td>$details->email</td><td>$details->phone</td><td>$details->address</td><td><a href='./coursework3edit.php?idx=$idx'>Edit</a></td><td><a href='./userDelet.php'>Del</a></td></tr>";
-                    }
+    <div class="User_Table">
+        <?php
+            function tabledisplay($details){
+                return "<tr><th scope='row'>$details->id</th><td>$details->first_name</td><td>$details->last_name</td><td>$details->user_type</td><td>$details->username</td><td>$details->email</td><td>$details->phone</td><td>$details->address</td><td><a href='./coursework3edit.php?idx=$idx'>Edit</a></td><td><a href='./userDelet.php'>Del</a></td></tr>";
+            }
 
-                    $fileHandler = fopen('./files/data.json','r');
-                    $data = fread($fileHandler,filesize('./files/data.json'));
-                    fclose($fileHandler);
-                    $userData = json_decode($data);
-                
+            $fileHandler = fopen('./files/data.json','r');
+            $data = fread($fileHandler,filesize('./files/data.json'));
+            fclose($fileHandler);
+            $userData = json_decode($data);
+        
 
 
-                    //sorting by usertype 'Dortor' or 'General' or 'Admin'
-                    echo "<form method='POST'>";
-                    echo "Type :  ";
-                    echo "<select name='dep' style='width: 200px;'>";
-                    echo "<option selected>All</option>";
-                    $depart = [];
-                    foreach($userData as $option){
-                        array_push($depart,$option->user_type);
-                    }
-                    $deparList=array_unique($depart);
-                    foreach($deparList as $list){
-                        echo "<option>$list</option>";
-                    }
-                    echo "</select>";
-                    echo "<button type='submit'>Sort</button>";
-                    echo "</form>";
+            //sorting by usertype 'Dortor' or 'General' or 'Admin'
+            echo "<form method='POST'>";
+            echo "Type :  ";
+            echo "<select name='dep' style='width: 200px;'>";
+            echo "<option selected>All</option>";
+            $depart = [];
+            foreach($userData as $option){
+                array_push($depart,$option->user_type);
+            }
+            $deparList=array_unique($depart);
+            foreach($deparList as $list){
+                echo "<option>$list</option>";
+            }
+            echo "</select>";
+            echo "<button type='submit'>Sort</button>";
+            echo "</form>";
 
-                    /*foreach($userData as $idx => $details){
-                        foreach($details as $key => $value){
-                            echo $key;
-                        }
-                    }
-                    */
-
-                    echo "<table border=1><thead><tr><th>Num</th><th>First Name</th><th>Last Name</th><th>User Type</th><th>ID</th><th>Email</th><th>Phone</th><th>Address</th><th>Edit</th><th>Delet</th></tr><thead>";
-                    echo "<tbody>";
-
+            /*foreach($userData as $idx => $details){
+                foreach($details as $key => $value){
+                    echo $key;
+                }
+            }
+            */
+        ?>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Num</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">User Type</th>
+                <th scope="col">ID</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Address</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delet</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+            <?php
                     // if it's not "POST" show everything
                     if($_SERVER['REQUEST_METHOD']!=="POST"){
                         foreach($userData as $idx => $details){
@@ -72,11 +87,13 @@
                             }
                         }
                     }
-                    echo "</tbody></table>";
                 ?>
-    </main>
-    <?php
-        
-    ?>
+            </tbody>
+        </table>
+    </div>
+</main>
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
